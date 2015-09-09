6f063fa84fba537563e23d4f5c61f37229d6fec0
Jerome LAURENT <jerome.laurent@orange.com>
2015-09-09 17:45:17 +0200

    developpement du service suite

--- TP0/js/app/controllers.js
+++ TP0/js/app/controllers.js
@@ -1,4 +1,5 @@
-var ContactListController = function ($scope) {
+var ContactListController = function ($scope, contactService) {
+    /*
     $scope.contacts = [ 
         { "id":0, "lastName":"Wayne",    "firstName":"Bruce",    "address":"Gotham city",               "phone":"555-BATMAN" },
         { "id":1, "lastName":"Parker",   "firstName":"Peter",    "address":"New York",                  "phone":"555-SPDRMN" },
@@ -9,22 +10,37 @@ var ContactListController = function ($scope) {
         { "id":6, "lastName":"Murdock",  "firstName":"Matt",     "address":"San Francisco",             "phone":"555-DARDVL" },
         { "id":7, "lastName":"Stark",    "firstName":"Tony",     "address":"Stark tower, New York",     "phone":"555-IRNMAN" }
     ];
+    */
+    $scope.contacts = contactService.getAllContacts();
 }
 var ContactEditController = function ($scope) {
 }
 
 var NavBarController = function ($scope, $location) {
     $scope.isActive = function(path) {
-        //return ($location.path() == path);
-        //return "la fonction indexOf('x') retourne la premiere position de x trouvé dans cette chaine de texte, soit ici 16".indexOf("x");
-        //return $location.absUrl(); // retourne l'adresse URL complete (only getter)- exemple: http://localhost:8080/#/some/path?foo=bar&baz=xoxo 
-        //return $location.url(); // retourne le path de l'adresse URL complete (getter/setter)- exemple: /some/path?foo=bar&baz=xoxo 
-        //return $location.protocol(); // exemple: http
-        //return $location.host(); // exemple: localhost
-        //return $location.port(); // exemple: 8080
-        //return $location.path(); // exemple: /some/path
-        //return $location.search(); // retourne l'objet GET - exemple: {foo: 'bar', baz: 'xoxo'}
-        //return $location.hash(); // // given url http://example.com/#/some/path?foo=bar&baz=xoxo#hashValue => "hashValue"
+        // "la fonction indexOf('x') retourne la premiere position de x trouvé dans cette chaine de texte, soit ici 16".indexOf("x");
+        /*
+            var lpath   = $location.path();
+            var labsUrl = $location.absUrl(); // retourne l'adresse URL complete (only getter)- exemple: http://localhost:8080/#/some/path?foo=bar&baz=xoxo 
+            var lurl    = $location.url(); // retourne le path de l'adresse URL complete (getter/setter)- exemple: /some/path?foo=bar&baz=xoxo 
+            var lproto  = $location.protocol(); // exemple: http
+            var lhost   = $location.host(); // exemple: localhost
+            var lport   = $location.port(); // exemple: 8080
+            var lpath   = $location.path(); // exemple: /some/path
+            var lsearch = $location.search(); // retourne l'objet GET - exemple: {foo: 'bar', baz: 'xoxo'}
+            var lhash   = $location.hash(); // // given url http://example.com/#/some/path?foo=bar&baz=xoxo#hashValue => "hashValue"
+            var infos   = "$location =";
+            infos += "=> .path():   "+lpath;
+            infos += " - .absUrl(): "+labsUrl;
+            infos += " - .url(): "   +lurl;
+            infos += " - .proto(): " +lproto;
+            infos += " - .host(): "  +lhost;
+            infos += " - .port(): "  +lport;
+            infos += " - .path(): "  +lpath;
+            infos += " - .search(): "+lsearch;
+            infos += " - .hash(): "  +lhash;
+            console.log(infos);
+        */
         return $location.path().indexOf(path) != -1;
     }
 }
--- TP0/js/app/services.js
+++ TP0/js/app/services.js
@@ -1,3 +1,42 @@
 'use strict';
 
-var zenContactServices = angular.module('zenContactServices',[]);
+var zenContactServices = angular.module('zenContactServices',[]); // Module correspondant à la couche service (découplage de l'application pour futur tests)
+
+zenContactServices.factory('contactService', function() { // déclaration du service contactService responsable de la gestion des contacts
+        var instance = {};
+
+        var contacts = [
+            { "id":0, "lastName":"Wayne",    "firstName":"Bruce",    "address":"Gotham city",               "phone":"555-BATMAN" },
+            { "id":1, "lastName":"Parker",   "firstName":"Peter",    "address":"New York",                  "phone":"555-SPDRMN" },
+            { "id":2, "lastName":"Storm",    "firstName":"Jane",     "address":"Baxter building, New York", "phone":"555-INVGRL" },
+            { "id":3, "lastName":"Richards", "firstName":"Red",      "address":"Baxter building, New York", "phone":"555-MRFANT" },
+            { "id":4, "lastName":"Storm",    "firstName":"Johnny",   "address":"Baxter building, New York", "phone":"555-TORCH"  },
+            { "id":5, "lastName":"Grimm",    "firstName":"Benjamin", "address":"Baxter building, New York", "phone":"555-THING"  },
+            { "id":6, "lastName":"Murdock",  "firstName":"Matt",     "address":"San Francisco",             "phone":"555-DARDVL" },
+            { "id":7, "lastName":"Stark",    "firstName":"Tony",     "address":"Stark tower, New York",     "phone":"555-IRNMAN" }
+        ];
+
+        instance.getAllContacts = function() {
+            var list_lastName =""
+            for (var key in contacts) {
+                list_lastName += (list_lastName!=""?", ":"") + contacts[key].lastName; // log dans la console
+            }
+            console.log("liste des lastName des contacts: "+list_lastName);
+            return contacts;
+        };
+
+        instance.getContactById = function(idParam) {
+                for (var i=0 ; i< contacts.length ; i++) {
+                    if (contacts[i].id === idParam) {
+                        return contacts[i];
+                    }
+                }
+                // return _.findWhere(contacts, {id: idParam})   //undeurscore.js
+        }
+
+        instance.saveContact = function(contact) {
+                    // TODO;
+        }
+
+        return instance;
+});
