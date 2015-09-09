bf32be5c81397de0c7b6e2ee953a48c85315cc5a
Jerome LAURENT <jerome.laurent@orange.com>
2015-09-09 16:11:09 +0200

    Navigation service $location d'AngularJs

--- TP0/js/app/controllers.js
+++ TP0/js/app/controllers.js
@@ -15,6 +15,16 @@ var ContactEditController = function ($scope) {
 
 var NavBarController = function ($scope, $location) {
     $scope.isActive = function(path) {
+        //return ($location.path() == path);
+        //return "la fonction indexOf('x') retourne la premiere position de x trouvé dans cette chaine de texte, soit ici 16".indexOf("x");
+        //return $location.absUrl(); // retourne l'adresse URL complete (only getter)- exemple: http://localhost:8080/#/some/path?foo=bar&baz=xoxo 
+        //return $location.url(); // retourne le path de l'adresse URL complete (getter/setter)- exemple: /some/path?foo=bar&baz=xoxo 
+        //return $location.protocol(); // exemple: http
+        //return $location.host(); // exemple: localhost
+        //return $location.port(); // exemple: 8080
+        //return $location.path(); // exemple: /some/path
+        //return $location.search(); // retourne l'objet GET - exemple: {foo: 'bar', baz: 'xoxo'}
+        //return $location.hash(); // // given url http://example.com/#/some/path?foo=bar&baz=xoxo#hashValue => "hashValue"
         return $location.path().indexOf(path) != -1;
     }
 }
