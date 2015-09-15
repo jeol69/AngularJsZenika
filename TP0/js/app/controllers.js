30ac60953a69ad50b2e9046da40a22bf40bb777f
Jerome LAURENT <jerome.laurent@orange.com>
2015-09-15 18:47:30 +0200

    Consommer une ressource Rest avec $resource

--- TP0/js/app/controllers.js
+++ TP0/js/app/controllers.js
@@ -1,5 +1,7 @@
-zenContactApp.controller('ContactListController', ['$scope', 'contactService', function ($scope, contactService) {
+//zenContactApp.controller('ContactListController', ['$scope', 'contactService', function ($scope, contactService) {
+zenContactApp.controller('ContactListController', ['$scope', 'sFcontact', function ($scope, sFcontact) {
     /*
+    // ex solution 1
     $scope.contacts = [ 
         { "id":0, "lastName":"Wayne",    "firstName":"Bruce",    "address":"Gotham city",               "phone":"555-BATMAN" },
         { "id":1, "lastName":"Parker",   "firstName":"Peter",    "address":"New York",                  "phone":"555-SPDRMN" },
@@ -10,16 +12,21 @@ zenContactApp.controller('ContactListController', ['$scope', 'contactService', f
         { "id":6, "lastName":"Murdock",  "firstName":"Matt",     "address":"San Francisco",             "phone":"555-DARDVL" },
         { "id":7, "lastName":"Stark",    "firstName":"Tony",     "address":"Stark tower, New York",     "phone":"555-IRNMAN" }
     ];
+    // ex solution 2
     $scope.contacts = contactService.getAllContacts();
-    */
+    // ex solution 3
     contactService.getAllContacts(function(data) {
         $scope.contacts = data;
     });
+    */
+    $scope.contacts = sFcontact.query();
 }]);
 
-zenContactApp.controller('ContactEditController', ['$scope', '$routeParams', '$location', 'contactService', function ($scope, $routeParams, $location, contactService) {
+//zenContactApp.controller('ContactEditController', ['$scope', '$routeParams', '$location', 'contactService', function ($scope, $routeParams, $location, contactService) {
+zenContactApp.controller('ContactEditController', ['$scope', '$routeParams', '$location', 'sFcontact', function ($scope, $routeParams, $location, sFcontact) {
     $scope.contact = {};
     /*
+    // ex solution 1
     if ($routeParams.id) {
         $scope.contact = contactService.getContactById(parseInt($routeParams.id));
         var info =  "$routeParams.id: " + $routeParams.id;
@@ -28,51 +35,47 @@ zenContactApp.controller('ContactEditController', ['$scope', '$routeParams', '$l
     } else {
         $scope.contact = {};
     }
-    */
+    // ex solution 2
     if ($routeParams.id) {
         //console.log("$routeParams.id: ",$routeParams.id);
         $scope.contact = contactService.getContactById($routeParams.id, function(data) {
             $scope.contact = data;
         });
     }
+    */
+    if ($routeParams.id) {
+        $scope.contact = sFcontact.get({id:$routeParams.id});
+    } else {
+        $scope.contact = new sFcontact();
+    }
     /*
+    // ex solution 1
     $scope.saveContact = function(contact) {
         contactService.saveContact(contact);
         $location.path("/list"); //redirige l'utilisateur vers la liste de contact
     }
-    */
+    // ex solution 2
     $scope.saveContact = function() {
         contactService.saveContact($scope.contact, function() {
             $location.path('/list');
         });
     };
+    */
+    $scope.saveContact = function (contact) {
+          if (contact.id) {
+              contact.$update(function(){
+                  $location.path("/list");
+              });
+          } else {
+              sFcontact.save(contact, function(){
+                  $location.path("/list");
+              });
+          }
+    }
 }]);
 
 zenContactApp.controller('NavBarController', ['$scope', '$location', function ($scope, $location) {
     $scope.isActive = function(path) {
-        // "la fonction indexOf('x') retourne la premiere position de x trouvé dans cette chaine de texte, soit ici 16".indexOf("x");
-        /*
-            var lpath   = $location.path();
-            var labsUrl = $location.absUrl(); // retourne l'adresse URL complete (only getter)- exemple: http://localhost:8080/#/some/path?foo=bar&baz=xoxo 
-            var lurl    = $location.url(); // retourne le path de l'adresse URL complete (getter/setter)- exemple: /some/path?foo=bar&baz=xoxo 
-            var lproto  = $location.protocol(); // exemple: http
-            var lhost   = $location.host(); // exemple: localhost
-            var lport   = $location.port(); // exemple: 8080
-            var lpath   = $location.path(); // exemple: /some/path
-            var lsearch = $location.search(); // retourne l'objet GET - exemple: {foo: 'bar', baz: 'xoxo'}
-            var lhash   = $location.hash(); // // given url http://example.com/#/some/path?foo=bar&baz=xoxo#hashValue => "hashValue"
-            var infos   = "$location =";
-            infos += "=> .path():   "+lpath;
-            infos += " - .absUrl(): "+labsUrl;
-            infos += " - .url(): "   +lurl;
-            infos += " - .proto(): " +lproto;
-            infos += " - .host(): "  +lhost;
-            infos += " - .port(): "  +lport;
-            infos += " - .path(): "  +lpath;
-            infos += " - .search(): "+lsearch;
-            infos += " - .hash(): "  +lhash;
-            console.log(infos);
-        */
         return $location.path().indexOf(path) != -1;
     }
 }]);
